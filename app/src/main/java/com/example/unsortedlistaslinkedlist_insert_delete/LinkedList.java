122ee1fd92ead596e338189f0db1323f1b3b961e
rashid-korko <benooo.sy.96@gmail.com>
2024-02-02 14:36:17 +0330

    3th Commit

---
+++ app/src/main/java/com/example/unsortedlistaslinkedlist_insert_delete/LinkedList.java
@@ -0,0 +1,55 @@
+package com.example.unsortedlistaslinkedlist_insert_delete;
+
+public class LinkedList {
+    Node head;
+
+    public LinkedList(Node head){
+        this.head = head;
+    }
+
+    void addToEnd(int data){
+        Node n = new Node(data);
+        if (head == null) {
+            head = n;
+        }
+        else{
+            Node curr = head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = n;
+        }
+    }
+
+    void addToStart(int data){
+        Node n = new Node(data);
+        n.next = head;
+        head = n;
+    }
+
+    Node deleteLast(){
+        Node curr = head;
+        if(curr == null || curr.next == null){
+            head = null;
+            return curr;
+        }
+        Node nextNode = curr.next;
+        while (curr.next != null) {
+            if (nextNode.next != null) {
+                curr.next = null;
+            }
+            curr = nextNode;
+            nextNode = nextNode.next;
+        }
+        return curr;
+    }
+
+    Node deleteStart(){
+        if (head != null) {
+            Node toDelete = head;
+            head = head.next;
+            return toDelete;
+        }
+        return null;
+    }
+}

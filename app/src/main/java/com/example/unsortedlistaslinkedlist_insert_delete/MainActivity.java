41efc82619b9bc62faaf94a6019465c8f7be61cf
M.balwi <mohamadbalwi50@gmail.com>
2024-02-03 06:00:09 +0330

    7th Commit

--- app/src/main/java/com/example/unsortedlistaslinkedlist_insert_delete/MainActivity.java
+++ app/src/main/java/com/example/unsortedlistaslinkedlist_insert_delete/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.unsortedlistaslinkedlist_insert_delete;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void StartingApp(View v){
+
+        Intent i = new Intent(this, Activity2.class);
+        startActivity(i);
     }
 }

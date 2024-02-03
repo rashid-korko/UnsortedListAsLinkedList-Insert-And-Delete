89aedf0d788f869e2ee3c4d09979a5d64f65dbec
rashid-korko <benooo.sy.96@gmail.com>
2024-02-03 06:59:47 +0330

    9th Commit

--- app/src/main/java/com/example/unsortedlistaslinkedlist_insert_delete/Activity2.java
+++ app/src/main/java/com/example/unsortedlistaslinkedlist_insert_delete/Activity2.java
@@ -2,7 +2,9 @@ package com.example.unsortedlistaslinkedlist_insert_delete;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+
+    public void DoOperation(View v){
+
+        Intent i2 = new Intent(this, Activity3.class);
+        startActivity(i2);
     }
 }

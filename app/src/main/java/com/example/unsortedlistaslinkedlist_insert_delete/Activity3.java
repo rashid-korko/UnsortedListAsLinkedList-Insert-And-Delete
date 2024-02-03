a0cc5ec6e6fcbec4cb1abe6944911f5f87d9ee5f
M.balwi <mohamadbalwi50@gmail.com>
2024-02-03 08:09:44 +0330

    10th Commit

--- app/src/main/java/com/example/unsortedlistaslinkedlist_insert_delete/Activity3.java
+++ app/src/main/java/com/example/unsortedlistaslinkedlist_insert_delete/Activity3.java
@@ -2,6 +2,7 @@ package com.example.unsortedlistaslinkedlist_insert_delete;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Activity3 extends AppCompatActivity {

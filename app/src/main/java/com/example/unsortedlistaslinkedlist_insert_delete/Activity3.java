7ae20427f17bc6d3694fbef897c1925e87b00d0d
rashid-korko <benooo.sy.96@gmail.com>
2024-02-03 11:46:19 +0330

    11th Commit

--- app/src/main/java/com/example/unsortedlistaslinkedlist_insert_delete/Activity3.java
+++ app/src/main/java/com/example/unsortedlistaslinkedlist_insert_delete/Activity3.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity3 extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class Activity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+
+
     }
 }
